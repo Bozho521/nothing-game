c947d174e42d8936a49c80939b35112f8ecb79fc
Bozho521 <bota@itu.dk>
2026-04-11 15:57:10 +0200

    Movement constraints + Sprinting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,8 +14,14 @@ public class PlayerMovement : MonoBehaviour, IInteractor
 
     [Header("Movement Stats")]
     public float speed = 6f;
+    public float sprintSpeed = 10f;
     public float gravity = -19.62f; 
     public float jumpHeight = 2f;
+
+    [Header("Movement Constraints")]
+    public bool canLookUpAndDown = false; 
+    public bool canJump = false;          
+    public bool canSprint = false;
 
     [Header("Camera & Look Settings")]
     public Transform cameraTransform; 
@@ -27,6 +33,7 @@ public class PlayerMovement : MonoBehaviour, IInteractor
     public InputAction jumpAction;
     public InputAction lookAction;
     public InputAction interactAction;
+    public InputAction sprintAction;
 
     [Header("UI Elements")]
     public GameObject deathScreenUI; 
@@ -60,6 +67,7 @@ public class PlayerMovement : MonoBehaviour, IInteractor
         jumpAction.Enable();
         lookAction.Enable();
         interactAction.Enable();
+        sprintAction.Enable();
     }
 
     private void OnDisable()
@@ -68,6 +76,7 @@ public class PlayerMovement : MonoBehaviour, IInteractor
         jumpAction.Disable();
         lookAction.Disable();
         interactAction.Disable();
+        sprintAction.Disable();
     }
 
     private void Update()
@@ -91,9 +100,12 @@ public class PlayerMovement : MonoBehaviour, IInteractor
 
         transform.Rotate(Vector3.up * lookInput.x * mouseSensitivity);
 
-        xRotation -= lookInput.y * mouseSensitivity;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        if (canLookUpAndDown)
+        {
+            xRotation -= lookInput.y * mouseSensitivity;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
     }
 
     private void HandleMovement()
@@ -106,15 +118,20 @@ public class PlayerMovement : MonoBehaviour, IInteractor
             velocity.y = -2f; 
         }
 
-        if (jumpAction.triggered && controller.isGrounded)
+        if (jumpAction.triggered && controller.isGrounded && canJump)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
-        Vector3 finalMovement = (move * speed) + (Vector3.up * velocity.y);
+        float currentSpeed = speed;
+        if (canSprint && sprintAction.IsPressed())
+        {
+            currentSpeed = sprintSpeed;
+        }
 
+        Vector3 finalMovement = (move * currentSpeed) + (Vector3.up * velocity.y);
         controller.Move(finalMovement * Time.deltaTime);
 
         CheckInteraction();
@@ -190,5 +207,13 @@ public class PlayerMovement : MonoBehaviour, IInteractor
     public void OnInteractComplete(IInteractable interacted)
     {
         throw new System.NotImplementedException();
+    }
+
+    public void UnlockMovementConstraints()
+    {
+        canLookUpAndDown = true;
+        canJump = true;
+        canSprint = true;
+        Debug.Log("Constraints unlocked!");
     }
 }
