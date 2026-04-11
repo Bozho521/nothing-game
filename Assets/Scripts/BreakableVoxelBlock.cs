63a8d39cb0c43b12219434c9248db07ee91e965e
jalr <jalr@itu.dk>
2026-04-11 19:12:36 +0100

    Fixed materials in script

--- Assets/Scripts/BreakableVoxelBlock.cs
+++ Assets/Scripts/BreakableVoxelBlock.cs
@@ -7,10 +7,39 @@ public class BreakableVoxelBlock : MonoBehaviour, IDestructable
     [SerializeField] private GameObject mainBlock;
 
     [SerializeField] private GameObject voxelBlocks;
+
+    private MeshRenderer _meshRenderer;
+
+    private Transform parentTransform;
+
+    private Transform brokenCubeParent;
+
+    private Material mainMaterial;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        parentTransform = transform.parent;
+        brokenCubeParent = parentTransform.Find("1_BROKEN CUBE");
+     
+        _meshRenderer = mainBlock.GetComponent<MeshRenderer>();
+        mainMaterial = _meshRenderer.materials[1];
+
+        foreach(var child in brokenCubeParent.GetComponentsInChildren<MeshRenderer>())
+        {
+            //Debug.Log("Found children with meshrenderer in broken cube parent");
+            var materials = child.sharedMaterials;
+            
+            for (int i = 0; i < materials.Length; i++)
+            {
+                if (materials[i].name == "Icon_Main")
+                {
+                    Debug.Log("ICON_MAIN FOUND");
+                    materials[i] = mainMaterial;
+                }
+            }
+
+            child.sharedMaterials = materials;
+        }
     }
 
     // Update is called once per frame
